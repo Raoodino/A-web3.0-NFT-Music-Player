bb59ae8055eb43d6fa510a028c11254c506eb699
jeriraooo <jerirao@foxmail.com>
2022-11-29 16:01:58 +0800

    updated

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # A-web3.0-NFT-Music-Player
 A block-chain based application enable artists receive royalty from their fans, and users could pay the selected music tokens using Ethereum 
+
+## Technology Stack & Tools
+- Solidity (Writing Smart Contract)
+- Javascript (React & Testing)
+- [Ethers](https://docs.ethers.io/v5/) (Blockchain Interaction)
+- [Hardhat](https://hardhat.org/) (Development Framework)
+- [IPFS](https://ipfs.io/) (Metadata storage)
+- [React routers](https://v5.reactrouter.com/) (Navigational components)
--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,18 +1,26 @@
 async function main() {
-
-  const [deployer] = await ethers.getSigners();
+  const toWei = (num) => ethers.utils.parseEther(num.toString())
+  let royaltyFee = toWei(0.01);
+  let prices = [toWei(1), toWei(2), toWei(3), toWei(4), toWei(5), toWei(6), toWei(7), toWei(8)]
+  let deploymentFees = toWei(prices.length * 0.01)
+  const [deployer, artist] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   // deploy contracts here:
   const NFTMarketplaceFactory = await ethers.getContractFactory("MusicNFTMarketplace");
-  const nftMarketplace = await NFTMarketplaceFactory.deploy();
+  nftMarketplace = await NFTMarketplaceFactory.deploy(
+    royaltyFee,
+    artist.address,
+    prices,
+    { value: deploymentFees }
+  );
 
-  console.log("Smart contract address:", nftMarketplace.address);
-  
+  console.log("Smart contract address:", nftMarketplace.address)
+
   // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles();
+  saveFrontendFiles(nftMarketplace, "MusicNFTMarketplace");
 }
 
 function saveFrontendFiles(contract, name) {
